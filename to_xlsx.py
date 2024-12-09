940b9296ad55fb2a7c5bd899dfe58433e6c8e3e5
admin <michael.hoarau@gmail.com>
2024-12-09 11:32:36 +0000

    Library update from DSS

--- to_xlsx.py
+++ to_xlsx.py
@@ -7,5 +7,6 @@ def dataframe_to_xlsx(input_dataframe, folder_name, output_file_name):
     folder_path = folder.get_path()
     folder_path = folder_path + "/" + output_file_name + ".xlsx"
     writer = pd.ExcelWriter(folder_path, engine="openpyxl")
-    input_dataframe.to_excel(writer, index=False, encoding="utf-9")
+    input_dataframe.to_excel(writer, index=False, encoding="iso 8859-1")
+    print("test")
     writer.save()
